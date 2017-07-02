fbd69c32b3baa71df06c20635668f004619e848b
Rob Cataneo <robcataneo@gmail.com>
2017-07-01 18:48:42 -0700

    Finished exercise 5 from strings and binaries

--- strings_and_binaries/ex5.exs
+++ strings_and_binaries/ex5.exs
@@ -2,19 +2,23 @@ defmodule MyString do
 
   def center([]), do: []
   def center(words) do
-    count = words
-     |> Enum.map_reduce(0, accumulate_max_length(&1, &2))
+    words
+     |> Enum.map_reduce(0, &accumulate_max_length/2)
      |> center_strings
      |> Enum.each(&IO.puts/1)
   end
 
-  defp accumulate_max_length(string, max) do
+  defp accumulate_max_length(string, max_length) do
     length = String.length(string)
-    { {string, length}, max(length, max) }
+    { {string, length}, max(length, max_length) }
   end
 
   defp center_strings({strings, max_length}) do
-    strings |> Enum.map(String.pad_leading())
+    strings |> Enum.map( &(center_string(max_length, &1)) )
+  end
 
+  defp center_string(max_length, {string, length}) do
+    new_length = div(max_length + length, 2)
+    String.pad_leading(string, new_length)
   end
 end
