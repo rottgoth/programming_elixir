577ac2d2bc8aa2d7a52ee6e63b7c0d44aaa0ecff
Rob Cataneo <robcataneo@gmail.com>
2018-05-19 15:42:25 -0700

    Refactor cli, added httpotion dep

--- ch13_project/issues/lib/issues/cli.ex
+++ ch13_project/issues/lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   that end up generating a table of the last _n_ issues in a github project.
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process()
   end
 
   @doc"""
@@ -19,14 +21,31 @@ defmodule Issues.CLI do
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      { [help: true], _, _ } -> :help
-      { _, [user, project, count], _ } -> { user, project, String.to_integer(count) }
-      { _, [user, project], _ } -> { user, project, @default_count }
-      _ -> :help
-    end
-    
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({ user, project, _count }) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
