a9d347f2455247dccab70b013bc2be53e8096015
Rob Cataneo <robcataneo@gmail.com>
2018-05-19 16:07:10 -0700

    Conver response, added potion library, and moved github url to config

--- ch13_project/issues/mix.exs
+++ ch13_project/issues/mix.exs
@@ -21,7 +21,8 @@ defmodule Issues.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpotion, "~> 1.0.0"}
+      {:httpotion, "~> 3.1.0"},
+      {:poison, "~> 3.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
