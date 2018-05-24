f0868cd85f898494310a5e4de050cbf1ef16a14e
Rob Cataneo <robcataneo@gmail.com>
2018-05-23 22:25:42 -0700

    Added coveralls to issues project

--- ch13_project/issues/mix.exs
+++ ch13_project/issues/mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.Mixfile do
       source: "https://github.com/rottgoth/issues",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
       deps: deps()
     ]
   end
@@ -28,7 +35,7 @@ defmodule Issues.Mixfile do
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.18.1"},
       {:earmark, "~> 1.2.4"},
-
+      {:excoveralls, "~> 0.8.2", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
